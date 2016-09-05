49125c5d932e19896d0c0d2d5374975252303c9f
Tim Lestander <timlestander@gmail.com>
2016-09-05 15:34:43 +0200

    Basic movement and setup

---
+++ Assets/Scripts/SnakeBody.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeBody : MonoBehaviour {
+
+	private int myOrder;
+	private Transform head;
+	private Vector3 movementVelocity;
+	[Range(0.0f,1.0f)]
+	public float overTime = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		head = GameObject.FindGameObjectWithTag ("Player").gameObject.transform;
+		for (int i = 0; i < head.GetComponent<SnakeMovement> ().bodyParts.Count; i++) {
+			if (gameObject == head.GetComponent<SnakeMovement> ().bodyParts [i].gameObject) {
+				myOrder = i;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (myOrder == 0) {
+			transform.position = Vector3.SmoothDamp (transform.position, head.position, ref movementVelocity, overTime);
+			transform.LookAt (head.transform.position);
+		} else {
+			transform.position = Vector3.SmoothDamp (transform.position, head.GetComponent<SnakeMovement> ().bodyParts [myOrder - 1].position, ref movementVelocity, overTime);
+			transform.LookAt (head.transform.position);
+		}
+	}
+}
---
+++ Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SnakeMovement : MonoBehaviour {
+
+	public List<Transform> bodyParts = new List<Transform> ();
+	public float rotationSensitivity = 50.0f;
+	public float speed = 3.5f;
+	public float boostCharge = 100f;
+	public Text boostText;
+
+	// Use this for initialization
+	void Start () {
+		boostText.text = "Boost: " + boostCharge + "%";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			currentRotation += rotationSensitivity * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			currentRotation -= rotationSensitivity * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.Space) && boostCharge > 1) {
+			speed = 7.0f;
+			boostCharge = boostCharge - 2f;
+			setBoostText ();
+		} else { 
+			if (boostCharge < 99) {
+				boostCharge = boostCharge + 0.1f;
+				setBoostText ();
+			}
+			speed = 3.5f;
+		}
+	}
+
+	void setBoostText() {
+		boostText.text = "Boost: " + Mathf.Ceil(boostCharge) + "%";
+	}
+
+	public float currentRotation;
+	void FixedUpdate() {
+		MoveForward ();
+		Rotation ();
+	}
+
+	void MoveForward() {
+		transform.position += transform.up * speed * Time.deltaTime;
+	}
+
+	void Rotation() {
+		transform.rotation = Quaternion.Euler (new Vector3 (transform.rotation.x, transform.rotation.y, currentRotation));
+	}
+
+	void Boost() {
+		speed = 7f;
+	}
+}
