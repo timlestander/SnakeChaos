d2c5eaf6399be9f074b548982e7d6c335738f47c
Tim Lestander <timlestander@gmail.com>
2016-09-16 09:58:40 +0200

    Cool features

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class SnakeMovement : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	public List<Transform> bodyParts = new List<Transform> ();
 	public float rotationSensitivity = 50.0f;
@@ -11,23 +11,29 @@ public class SnakeMovement : MonoBehaviour {
 	public float boostCharge = 100f;
     public bool powerupTriggered = false;
 
-	public KeyCode leftTurn;
-	public KeyCode rightTurn;
-	public KeyCode boostKey;
+	KeyCode leftTurn;
+	KeyCode rightTurn;
+	KeyCode boostKey;
 
-	Text boostText;
 	Rect boostRect;
 	Texture2D boostTexture;
+
+	bool isIt = true;
+
+	public void setUp(KeyCode left, KeyCode right, KeyCode boost) {
+		leftTurn = left;
+		rightTurn = right;
+		boostKey = boost;
+	}
 
 	// Use this for initialization
 	void Start () {
 		SpawnPlayer ();
-		boostText.text = "Boost: " + boostCharge + "%";
-
-		boostRect = new Rect (Screen.width / 2, Screen.height / 2, Screen.width / 3, Screen.height / 50);
+		boostRect = new Rect (20, 20, 100, 5);
 		boostTexture = new Texture2D (1, 1);
 		boostTexture.SetPixel (0, 0, Color.red);
 		boostTexture.Apply ();
+		StartCoroutine ("MakeItGlow");
 	}
 
 	// Update is called once per frame
@@ -47,10 +53,9 @@ public class SnakeMovement : MonoBehaviour {
             {
                 speed = 7.0f;
                 boostCharge = boostCharge - 2f;
-                setBoostText();
             }
 
-            if (Input.GetKeyUp(boostKey))
+			if (Input.GetKeyUp(boostKey) || boostCharge < 1)
             {
 
                 speed = 3.5f;
@@ -59,7 +64,6 @@ public class SnakeMovement : MonoBehaviour {
 
         if (boostCharge < 99) {
 			boostCharge = boostCharge + 0.1f;
-			setBoostText ();
 		}
 	}
 
@@ -95,10 +99,6 @@ public class SnakeMovement : MonoBehaviour {
 		}
 	}
 
-	void setBoostText() {
-		boostText.text = "Boost: " + Mathf.Ceil(boostCharge) + "%";
-	}
-
 	public float currentRotation;
 	void FixedUpdate() {
 		MoveForward ();
@@ -115,7 +115,7 @@ public class SnakeMovement : MonoBehaviour {
 
 	void OnGUI() {
 		float ratio = boostCharge / 100;
-		float rectWidth = ratio * Screen.width / 3;
+		float rectWidth = ratio * 100;
 		boostRect.width = rectWidth;
 		GUI.DrawTexture (boostRect, boostTexture);
 	}
@@ -132,4 +132,31 @@ public class SnakeMovement : MonoBehaviour {
         speed = 3.5f;
         powerupTriggered = false;
     }
+
+	float lerpTime = 1f;
+	IEnumerator MakeItGlow() { 
+
+		while (isIt) {
+			
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.Lerp(Color.white, Color.black, lerpTime);
+			foreach (Transform body in bodyParts) {
+				body.GetComponent<SpriteRenderer> ().color = Color.Lerp(Color.white, Color.black, lerpTime);
+			}
+
+			yield return new WaitForSeconds (lerpTime);
+
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.Lerp(Color.black, Color.white, lerpTime);
+			foreach (Transform body in bodyParts) {
+				body.GetComponent<SpriteRenderer> ().color = Color.Lerp(Color.black, Color.white, lerpTime);
+			}
+
+			yield return new WaitForSeconds (lerpTime);
+
+			foreach (Transform body in bodyParts) {
+				body.GetComponent<SpriteRenderer> ().color = Color.Lerp (Color.white, Color.black, lerpTime);
+			}
+
+			yield return true;
+		}
+	}
 }
